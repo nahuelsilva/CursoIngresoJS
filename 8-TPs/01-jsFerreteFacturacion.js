8f1c52659881f2bdfd15ea731aa5907652d96578
nahuelsilva <nahuelchueco.12.20@gmail.com>
2020-07-16 19:12:28 -0300

    tp uno y dos terminados

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,74 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    var precioUno;
+    var precioDos;
+    var precioTres;
+    var sumar;
+
+    precioUno = txtIdPrecioUno.value;
+    precioUno = parseInt(precioUno);
+
+    precioDos = txtIdPrecioDos.value;
+    precioDos = parseInt(precioDos);
+
+    precioTres = txtIdPrecioTres.value;
+    precioTres = parseInt(precioTres);
+
+    sumar = precioUno + precioDos + precioTres;
+    sumar = parseInt(sumar);
+
+    alert("La suma es: " + sumar);
+
+
 }
 function Promedio () 
 {
-	
+    var precioUno;
+    var precioDos;
+    var precioTres;
+    var promedio;
+
+    precioUno = txtIdPrecioUno.value;
+    precioUno = parseInt(precioUno);
+
+    precioDos = txtIdPrecioDos.value;
+    precioDos = parseInt(precioDos);
+
+    precioTres = txtIdPrecioTres.value;
+    precioTres = parseInt(precioTres);
+
+    promedio = (precioUno + precioDos + precioTres) / 3;
+
+    alert("El promedio es: " + promedio);
+
 }
 function PrecioFinal () 
 {
-	
+    
+    var precioUno;
+    var precioDos;
+    var precioTres;
+    var suma;
+    var iva;
+    var PrecioFinal;
+
+    precioUno = document.getElementById('txtIdPrecioUno').value;
+    precioUno = parseInt(precioUno);
+
+    precioDos = document.getElementById('txtIdPrecioDos').value;
+    precioDos = parseInt(precioDos);
+
+    precioTres = document.getElementById('txtIdPrecioTres').value;
+    precioTres = parseInt(precioTres);
+
+    suma = precioUno + precioDos + precioTres;
+    suma = parseInt(suma);
+
+    //iva = suma * 0,21; ---- preguntar xq no multiplica el 0,21 
+    iva = (suma * 21) / 100;
+
+    PrecioFinal = suma + iva;
+    alert("El precio final es: " + PrecioFinal);
+
 }
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -7,12 +7,69 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 function Rectangulo () 
 {
 
+    var largo;
+    var ancho;
+    var perimetro;
+    var cantidadfinal;
+
+    largo = txtIdLargo.value;
+    largo = parseInt(largo);
+
+    ancho = txtIdAncho.value;
+    ancho = parseInt(ancho);
+
+    perimetro = 2 * largo + 2 * ancho;
+    
+    cantidadfinal = perimetro * 3;
+
+
+    alert ("La cantidad de alambre es: " + cantidadfinal + " metros");
+
 }
 function Circulo () 
 {
-	
+    var radio;
+    var circunferencia;
+    var alambre;
+
+    radio = txtIdRadio.value;
+    radio = parseInt(radio);
+
+    circunferencia = (2 * radio) * 3,1415;
+
+    alambre = circunferencia * 3; 
+
+    alert("La cantidad de alambre para el circulo es: " + alambre + " metros");
+
 }
 function Materiales () 
 {
-	
+    var largo;
+    var ancho;
+    var area;
+    var cemento;
+    var cal;
+
+    largo = txtIdLargo.value;
+    largo = parseInt(largo);
+
+    ancho = txtIdAncho.value;
+    ancho = parseInt(ancho);
+
+    area = largo * ancho;
+    area = parseInt(area);
+
+    cemento = area * 2;
+    cemento = parseInt(cemento);
+
+    cal = area * 3;
+    cal = parseInt(cal);
+
+    alert("El area a cubrir es de: " + area + "metros cuadrados");
+
+    alert("Se debe usar de cal: " + cal + " bolsas" + " y de cemento: " + cemento + " bolsas");
+
+
+
+    
 }
